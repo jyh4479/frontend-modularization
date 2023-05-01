110dfedf7aeb329c5dfb0e4399bae1d45f596132
jyh4479 <jyh.dev@gmail.com>
2023-05-01 15:41:36 +0900

    잘못된 요구사항 개발

--- src/step3(src)/components/Payment.tsx
+++ src/step3(src)/components/Payment.tsx
@@ -1,3 +1,5 @@
+import {useState, useMemo} from "react";
+
 import {usePaymentMethods} from "../hooks/usePaymentMethods";
 import {PaymentMethods} from "../components/PaymentMethods";
 
@@ -5,11 +7,35 @@ export const Payment = ({amount}: { amount: number }) => {
 
     const paymentMethods = usePaymentMethods();
 
+    const [agreeToDonate, setAgreeToDonate] = useState<boolean>(false);
+
+    const {total, tip} = useMemo(
+        () => ({
+            total: agreeToDonate ? Math.floor(amount + 1) : amount,
+            tip: parseFloat((Math.floor(amount + 1) - amount).toPrecision(10)),
+        }),
+        [amount, agreeToDonate]
+    );
+
     return (
         <div>
             <h3>Payment</h3>
             <PaymentMethods paymentMethods={paymentMethods}/>
-            <button>${amount}</button>
+            <div>
+                <label>
+                    <input
+                        type="checkbox"
+                        onChange={handleChange}
+                        checked={agreeToDonate}
+                    />
+                    <p>
+                        {agreeToDonate
+                            ? "Thanks for your donation."
+                            : `I would like to donate $${tip} to charity.`}
+                    </p>
+                </label>
+            </div>
+            <button>${total}</button>
         </div>
     );
 };
