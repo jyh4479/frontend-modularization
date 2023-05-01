29c0e98222858df8f5af3555880194f0aff9891f
jyh4479 <jyh.dev@gmail.com>
2023-05-01 15:52:57 +0900

    hook 추출 및 component 분리

---
+++ src/step3(src)/components/DonationCheckbox.tsx
@@ -0,0 +1,10 @@
+export const DonationCheckbox = ({onChange, checked, content,}: DonationCheckboxProps) => {
+    return (
+        <div>
+            <label>
+                <input type="checkbox" onChange={onChange} checked={checked}/>
+                <p>{content}</p>
+            </label>
+        </div>
+    );
+};
--- src/step3(src)/components/Payment.tsx
+++ src/step3(src)/components/Payment.tsx
@@ -1,40 +1,28 @@
-import {useState, useMemo} from "react";
-
 import {usePaymentMethods} from "../hooks/usePaymentMethods";
 import {PaymentMethods} from "../components/PaymentMethods";
+import {useRoundUp} from "../hooks/useRoundUp";
+import {DonationCheckbox} from "./DonationCheckbox";
 
 export const Payment = ({amount}: { amount: number }) => {
 
     const paymentMethods = usePaymentMethods();
+    const {total, tip, agreeToDonate, updateAgreeToDonate} = useRoundUp(amount);
 
-    const [agreeToDonate, setAgreeToDonate] = useState<boolean>(false);
-
-    const {total, tip} = useMemo(
-        () => ({
-            total: agreeToDonate ? Math.floor(amount + 1) : amount,
-            tip: parseFloat((Math.floor(amount + 1) - amount).toPrecision(10)),
-        }),
-        [amount, agreeToDonate]
-    );
+    const formatCheckboxLabel = (agreeToDonate: boolean, tip: number) => {
+        return agreeToDonate
+            ? "Thanks for your donation."
+            : `I would like to donate $${tip} to charity.`;
+    };
 
     return (
         <div>
             <h3>Payment</h3>
             <PaymentMethods paymentMethods={paymentMethods}/>
-            <div>
-                <label>
-                    <input
-                        type="checkbox"
-                        onChange={handleChange}
-                        checked={agreeToDonate}
-                    />
-                    <p>
-                        {agreeToDonate
-                            ? "Thanks for your donation."
-                            : `I would like to donate $${tip} to charity.`}
-                    </p>
-                </label>
-            </div>
+            <DonationCheckbox
+                onChange={updateAgreeToDonate}
+                checked={agreeToDonate}
+                content={formatCheckboxLabel(agreeToDonate, tip)}
+            />
             <button>${total}</button>
         </div>
     );
